c81fc3a5208938c29e885e78232c8780f4fc92e2
Belisy <lssoy77@gmail.com>
2022-11-22 23:49:58 +0900

    Test

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,10 +1,12 @@
 import { useState, useEffect, useRef, useCallback, useMemo } from "react";
 import { useRouter } from "next/router";
 import styled from "@emotion/styled";
-import { categories, products } from "@prisma/client";
+import { categories, products, User } from "@prisma/client";
 import { useQuery } from "@tanstack/react-query";
 import Image from "next/image";
 import useDebounce from "hooks/useDebounce";
+import { userAgent } from "next/server";
+import UserInfo from "components/UserInfo";
 
 // TODO: 코드 리팩토링 및 컴포넌트화 필요
 
