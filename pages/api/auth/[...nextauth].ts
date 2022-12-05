17ff862ba1b9490d9871c126909281cea7fc158c
Belisy <lssoy77@gmail.com>
2022-12-06 06:28:35 +0900

    사용하지 않는 모듈제거

--- pages/api/auth/[...nextauth].ts
+++ pages/api/auth/[...nextauth].ts
@@ -1,5 +1,5 @@
 import GoogleProvider from "next-auth/providers/google";
-import { PrismaClient, Session } from "@prisma/client";
+import { PrismaClient } from "@prisma/client";
 import NextAuth, { NextAuthOptions } from "next-auth";
 import { PrismaAdapter } from "@next-auth/prisma-adapter";
 
